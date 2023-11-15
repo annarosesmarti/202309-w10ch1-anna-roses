3e9f8a3f963f6f2c9634b73826b75e060e797fdb
Anna Roses <anna.roses.marti@gmail.com>
2023-11-15 12:21:40 +0100

    Add getThingsById in thingsRouter

--- src/features/things/router/thingsRouter.ts
+++ src/features/things/router/thingsRouter.ts
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import things from "../data/things.js";
 import ThingsController from "../controller/ThingsController.js";
 
 const thingsRouter = Router();
@@ -9,21 +8,5 @@ const thingsController = new ThingsController();
 thingsRouter.get("/", thingsController.getThings);
 
 thingsRouter.get("/:idThings", thingsController.getThingsById);
-
-thingsRouter.delete("/:idThing", (req, res) => {
-  const id = +req.params.idThing;
-
-  const thingIndex = things.findIndex((thing) => thing.id === id);
-
-  if (thingIndex === -1) {
-    res.status(404).json({ error: "Thing not found" });
-
-    return;
-  }
-
-  things.splice(thingIndex, 1);
-
-  res.status(200).json({});
-});
 
 export default thingsRouter;
